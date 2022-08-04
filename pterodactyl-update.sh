19151f03b0c97717a05d49d0680b61f1602860cb
TeALO36 <83779026+TeALO36@users.noreply.github.com>
2022-08-04 15:31:27 +0200

    Add files via upload

---
+++ pterodactyl-update.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+
+echo Welcome to pterodactyl update.
+
+cd /var/www/pterodactyl
+
+php artisan down
+
+curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
+
+chmod -R 755 storage/* bootstrap/cache
+
+composer install --no-dev --optimize-autoloader -n
+
+php artisan view:clear
+php artisan config:clear
+
+php artisan migrate --seed --force
+
+chown -R www-data:www-data /var/www/pterodactyl/*
+
+php artisan queue:restart
+
+php artisan up
